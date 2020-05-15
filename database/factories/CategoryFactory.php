6b18cb7290715175169399ef64c68d5324cd52a6
abdollah zarrin <abdollahzarrin@yahoo.com>
2020-05-15 13:09:26 +0430

    Factory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,12 +1,14 @@
 <?php
-
+use Illuminate\Support\Str;
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Model\Category;
 use Faker\Generator as Faker;
 
 $factory->define(Category::class, function (Faker $faker) {
+    $name = $faker -> word;
     return [
-        //
+        'name' => $name,
+        'slug' => Str::slug($name)
     ];
 });
