4b75631622ead7d4f9c5f45f7e2049b2e16249a2
abdollah zarrin <abdollahzarrin@yahoo.com>
2020-05-15 22:52:51 +0430

    Add JWT And Functions

--- routes/api.php
+++ routes/api.php
@@ -7,3 +7,15 @@ Route::apiResource('/question/{question}/reply','ReplyController');
 
 Route::post('like/{reply}','LikeController@like');
 Route::delete('like/{reply}','LikeController@unLike');
+Route::group([
+
+    'prefix' => 'auth'
+
+], function () {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
